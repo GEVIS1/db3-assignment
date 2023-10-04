6ceccc9bfb48dbd2a02db3aa094dd961895e4db1
Steffen Geving <gevis1@student.op.ac.nz>
2023-10-04 18:19:56 +1300

    feat: add QuoteComponent table

--- gevis1_IN705Assignment1/Create Tables.sql
+++ gevis1_IN705Assignment1/Create Tables.sql
@@ -61,6 +61,49 @@ CREATE TABLE Quote
 )
 GO
 
+IF OBJECT_ID('QuoteComponent', 'U') IS NOT NULL
+  DROP TABLE QuoteComponent
+GO
+
+CREATE TABLE QuoteComponent
+(
+	ComponentID INTEGER NOT NULL,
+	QuoteID INTEGER NOT NULL,
+
+	-- DECIMAL(9,2) gives a 5 byte size with a max number of 9999999.99 which is sufficient
+	Quantity DECIMAL(9,2) DEFAULT 1,
+	TradePrice MONEY NOT NULL,
+	ListPrice MONEY NOT NULL,
+	TimeToFit DECIMAL DEFAULT 0,
+	
+	CONSTRAINT FK_QuoteComponent_Component
+		FOREIGN KEY (ComponentID)
+		REFERENCES Component(ComponentID)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION, -- Will be handled by trigger
+
+	CONSTRAINT FK_QuoteComponent_Quote
+		FOREIGN KEY (QuoteID)
+		REFERENCES Quote(QuoteID)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+
+	CONSTRAINT PK_QuoteComponent
+		PRIMARY KEY (ComponentID, QuoteID),
+
+	CONSTRAINT Valid_Quantity
+		CHECK (0 <= Quantity),
+
+	CONSTRAINT Valid_TradePrice
+		CHECK (0 <= TradePrice),
+
+	CONSTRAINT Valid_ListPrice
+		CHECK (0 <= ListPrice),
+
+	CONSTRAINT Valid_TimeToFit
+		CHECK (0 <= TimeToFit),
+)
+GO
 
 ---- Drop table if it exists
 --IF OBJECT_ID('<TableName>', 'U') IS NOT NULL
