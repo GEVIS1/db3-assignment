5e9147d73ecb198525f75e3404e65a5b852912ef
Steffen Geving <gevis1@student.op.ac.nz>
2023-10-07 19:13:50 +1300

    feat: add procedure addSubComponent

--- gevis1_IN705Assignment1/4 Create Procedures.sql
+++ gevis1_IN705Assignment1/4 Create Procedures.sql
@@ -10,3 +10,24 @@ BEGIN
 		VALUES (@ComponentName,	@ComponentDescription, 0, 0, 0, dbo.getCategoryID('Assembly'), dbo.getAssemblySupplierID())
 	RETURN 0 
 END
+GO
+
+CREATE OR ALTER PROCEDURE dbo.addSubComponent
+    @AssemblyName VARCHAR(50),
+    @SubComponentName VARCHAR(50),
+	@Quantity INTEGER
+AS
+BEGIN
+	DECLARE @AssemblyID INTEGER = (
+		SELECT DISTINCT TOP(1) ComponentID FROM Component WHERE ComponentName = @AssemblyName
+	);
+
+	DECLARE @SubComponentID INTEGER = (
+		SELECT DISTINCT TOP(1) ComponentID FROM Component WHERE ComponentName = @SubComponentName
+	);
+
+	INSERT INTO AssemblySubComponent (AssemblyID, SubcomponentID, Quantity)
+		VALUES (@AssemblyID, @SubComponentID, @Quantity)
+	RETURN 0
+END
+GO
