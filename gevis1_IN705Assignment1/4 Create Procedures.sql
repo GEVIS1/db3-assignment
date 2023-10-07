cb7608aee46c4a741ea50772d49788ddcf1a528c
Steffen Geving <gevis1@student.op.ac.nz>
2023-10-07 18:58:24 +1300

    chore: rename files

--- gevis1_IN705Assignment1/4 Create Procedures.sql
+++ gevis1_IN705Assignment1/4 Create Procedures.sql
@@ -6,7 +6,7 @@ CREATE OR ALTER PROCEDURE dbo.createAssembly
     @ComponentDescription VARCHAR(100)  
 AS
 BEGIN
-	INSERT INTO Component (@ComponentName, ComponentDescription, TradePrice, ListPrice, TimeToFit, CategoryID, SupplierID)
+	INSERT INTO Component (ComponentName, ComponentDescription, TradePrice, ListPrice, TimeToFit, CategoryID, SupplierID)
 		VALUES (@ComponentName,	@ComponentDescription, 0, 0, 0, dbo.getCategoryID('Assembly'), dbo.getAssemblySupplierID())
 	RETURN 0 
 END
