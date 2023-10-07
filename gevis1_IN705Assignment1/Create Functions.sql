99f7db87bc47d1b7d8f28d2dfff10391a1f50b7a
Steffen Geving <gevis1@student.op.ac.nz>
2023-10-07 18:24:20 +1300

    feat: add Create Functions.sql file
    this file holds the functions and currently has the getCategoryID
    function

---
+++ gevis1_IN705Assignment1/Create Functions.sql
@@ -0,0 +1,16 @@
+USE gevis1_IN705Assignment1;
+GO
+
+CREATE OR ALTER FUNCTION dbo.getCategoryID
+(
+    @CategoryName VARCHAR(20) -- Same size as CategoryName field
+)
+RETURNS INTEGER
+AS
+BEGIN
+    RETURN (SELECT DISTINCT TOP(1)
+		CategoryID
+	FROM Category
+	WHERE CategoryName = @CategoryName)
+END
+GO
