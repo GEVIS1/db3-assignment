74d361db7c486f8b1472320ae7e5eb9ad5cf3a51
Steffen Geving <gevis1@student.op.ac.nz>
2023-10-07 18:50:58 +1300

    feat: add Create Procedures.sql
    this file adds the createAssembly procedure

---
+++ gevis1_IN705Assignment1/Create Procedures.sql
@@ -0,0 +1,12 @@
+USE gevis1_IN705Assignment1;
+GO
+
+CREATE OR ALTER PROCEDURE dbo.createAssembly
+    @ComponentName VARCHAR(50),
+    @ComponentDescription VARCHAR(100)  
+AS
+BEGIN
+	INSERT INTO Component (@ComponentName, ComponentDescription, TradePrice, ListPrice, TimeToFit, CategoryID, SupplierID)
+		VALUES (@ComponentName,	@ComponentDescription, 0, 0, 0, dbo.getCategoryID('Assembly'), dbo.getAssemblySupplierID())
+	RETURN 0 
+END
