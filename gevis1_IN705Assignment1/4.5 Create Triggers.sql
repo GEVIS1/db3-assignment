abcddcdf4a427008bfa48ebbb29881c5fad3b35a
Steffen Geving <gevis1@student.op.ac.nz>
2023-10-08 23:09:37 +1300

    feat: add progress on FK_Assembly_Component

--- gevis1_IN705Assignment1/4.5 Create Triggers.sql
+++ gevis1_IN705Assignment1/4.5 Create Triggers.sql
@@ -1,6 +1,99 @@
 USE gevis1_IN705Assignment1;
 GO
 
+
+--CREATE OR ALTER TRIGGER trigFK_Assembly_Component
+--ON Component
+--AFTER UPDATE
+--AS
+--	-- Is there a better way to do this?
+--	/*
+--	-- Store the assembly components' old IDs before updating Component
+--	DECLARE @Assemblies TABLE (AssemblyID INTEGER, CategoryID INTEGER);
+
+--	INSERT INTO @Assemblies
+--		SELECT i.ComponentID, i.CategoryID 
+--		FROM inserted AS i
+--		JOIN Category AS c ON i.CategoryID = c.CategoryID
+--		WHERE c.CategoryName = 'Assembly';
+
+--	-- Run the actual update statement
+--	UPDATE Component
+--	SET 
+--		ComponentID = CASE 
+--			WHEN i.ComponentID <> c.ComponentID THEN i.ComponentID
+--			ELSE c.ComponentID END,
+--		ComponentName = CASE
+--			WHEN i.ComponentName <> c.ComponentName THEN i.ComponentName
+--			ELSE c.ComponentName END,
+--		ComponentDescription = CASE
+--			WHEN i.ComponentDescription <> c.ComponentDescription THEN i.ComponentDescription
+--			ELSE c.ComponentDescription END,
+--		TradePrice = CASE
+--			WHEN i.TradePrice <> c.TradePrice THEN i.TradePrice
+--			ELSE c.TradePrice END,
+--		ListPrice = CASE
+--			WHEN i.ListPrice <> c.ListPrice THEN i.ListPrice
+--			ELSE c.ListPrice END,
+--		TimeToFit = CASE
+--			WHEN i.TimeToFit <> c.TimeToFit THEN i.TimeToFit
+--			ELSE c.TimeToFit END,
+--		CategoryID = CASE
+--			WHEN i.CategoryID <> c.CategoryID THEN i.CategoryID
+--			ELSE c.CategoryID END,
+--		SupplierID = CASE
+--			WHEN i.SupplierID <> c.SupplierID THEN i.SupplierID
+--			ELSE c.SupplierID END
+--	FROM inserted AS i
+--	JOIN Component AS c ON i.ComponentID = c.ComponentID
+--	WHERE c.ComponentID = i.ComponentID;
+
+--	-- Update the sub components
+--	UPDATE AssemblySubComponent
+--	SET
+--		AssemblyID = inserted.ComponentID
+--	FROM AssemblySubComponent AS ac
+--	JOIN @Assemblies AS a ON ac.AssemblyID = a.AssemblyID
+--	WHERE a.AssemblyID = ac.AssemblyID
+
+--	*/
+
+--	-- Yes.. The old IDS are in Deleted and the new in Inserted.
+--	DECLARE @UpdatedAssemblies TABLE (
+--		OldAssemblyComponentID INTEGER,
+--		NewAssemblyComponentID INTEGER
+--	);
+
+--	--INSERT INTO @UpdatedAssemblies
+--	SELECT
+--		d.ComponentID AS OldAssemblyComponentID
+--	FROM deleted AS d
+--	UNION 
+--	SELECT
+--		i.ComponentID AS NewAssemblyComponentID
+--	FROM inserted AS i;
+
+--	--SELECT * FROM @UpdatedAssemblies;
+--;
+--GO
+
+--BEGIN TRANSACTION
+--SET IDENTITY_INSERT Component ON
+--UPDATE Component
+--SET
+--	ComponentID = 30959
+--WHERE ComponentID = 30950;
+--SET IDENTITY_INSERT Component OFF
+--ROLLBACK TRANSACTION
+--GO
+
+--CREATE OR ALTER TRIGGER trigFK_Subcomponent_Component
+--ON AssemblySubComponent
+--AFTER UPDATE
+--AS
+--;
+GO
+
 CREATE OR ALTER TRIGGER trigSupplierDelete
 ON Supplier
 INSTEAD OF DELETE
