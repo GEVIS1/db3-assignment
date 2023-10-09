63edc14c2626f2962e17ebb40f59e0898547de8c
Steffen Geving <gevis1@student.op.ac.nz>
2023-10-09 20:04:23 +1300

    feat: update createAssembly to use sequence

--- gevis1_IN705Assignment1/4 Create Procedures.sql
+++ gevis1_IN705Assignment1/4 Create Procedures.sql
@@ -6,8 +6,8 @@ CREATE OR ALTER PROCEDURE dbo.createAssembly
     @ComponentDescription VARCHAR(100)  
 AS
 BEGIN
-	INSERT INTO Component (ComponentName, ComponentDescription, TradePrice, ListPrice, TimeToFit, CategoryID, SupplierID)
-		VALUES (@ComponentName,	@ComponentDescription, 0, 0, 0, dbo.getCategoryID('Assembly'), dbo.getAssemblySupplierID())
+	INSERT INTO Component (ComponentID, ComponentName, ComponentDescription, TradePrice, ListPrice, TimeToFit, CategoryID, SupplierID)
+	SELECT NEXT VALUE FOR [dbo].ComponentID, @ComponentName, @ComponentDescription, 0, 0, 0, dbo.getCategoryID('Assembly'), dbo.getAssemblySupplierID()
 	RETURN 0 
 END
 GO
@@ -116,7 +116,6 @@ BEGIN
 		@CurrentListPrice  = ListPrice,
 		@CurrentTimeToFit  = TimeToFit
 	FROM Component WHERE ComponentID = @ComponentID
-
 	INSERT INTO QuoteComponent (
 		ComponentID,
 		QuoteID,
